fb0122ae4f3d081b9959512146da6f308619bf18
kidan16 <eyueltekle@hotmail.com>
2022-08-03 19:44:32 +0300

    classes name corrected

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,16 @@ This module stores and reloads objects of
 class 'Basemodel' or it's sub classes.
 """
 import json
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class FileStorage:
@@ -44,4 +54,5 @@ class FileStorage:
             for key in json_object.keys():
                 class_name = json_object[key]["__class__"]
                 object_dict = json_object[key]
-                self.__objects[key] = class_name(**object_dict)
+                self.__objects[key] = classes[class_name](**object_dict)
+
