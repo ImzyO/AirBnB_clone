8e8299d2705e425a40782367243385edc1e802a8
kidan16 <eyueltekle@hotmail.com>
2022-08-04 11:39:50 +0300

    add object to storage

--- models/base_model.py
+++ models/base_model.py
@@ -40,7 +40,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        storage.new()
+        storage.new(self)
 
     def __str__(self):
         """Returns a string representation of BaseModel instances"""
