71bf39536ecae464c1dcdeacb62e26436be76a43
kidan16 <eyueltekle@hotmail.com>
2022-08-07 18:13:06 +0300

    correction

--- models/base_model.py
+++ models/base_model.py
@@ -44,10 +44,9 @@ class BaseModel:
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of the instance"""
-        new_dict = self.__dict__.copy()
-        if "created_at" in new_dict:
-            new_dict["created_at"] = new_dict["created_at"].strftime(time)
-        if "updated_at" in new_dict:
-            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
-        new_dict["__class__"] = self.__class__.__name__
-        return new_dict
+        dictionary = {"__class__": self.__class__.__name__, "id": self.id}
+        if hasattr(self, "created_at")
+            dictionary["created_at"] = self.created_at.isoformat()
+        if hasattr(self, "updated_at")
+            dictionary["updated_at"] = self.updated_at.isoformat()
+        return dictionary
