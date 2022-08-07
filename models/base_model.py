447c0f1fdd2569935d185f6fa1d0503fe335429c
kidan16 <eyueltekle@hotmail.com>
2022-08-07 18:02:24 +0300

    correction

--- models/base_model.py
+++ models/base_model.py
@@ -19,10 +19,13 @@ class BaseModel:
             for key in kwargs.keys():
                 if key in ["created_at", "updated_at"]:
                     kwargs[key] = datetime.strptime(kwargs[key], tdf)
+                    if key == "created_at":
+                        self.created_at = kwargs["created_at"]
+                    if key == "updated_at":
+                        self.updated_at = kwargs["updated_at"]
+                if "id" in kwargs.keys():
+                    self.id = kwargs["id"]
 
-            self.id = kwargs["id"]
-            self.created_at = kwargs["created_at"]
-            self.updated_at = kwargs["updated_at"]
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
