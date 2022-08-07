e3eed5ffa987ca415798ff3defb00cf7df4adb1e
kidan16 <eyueltekle@hotmail.com>
2022-08-07 18:47:25 +0300

    correction

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,9 +11,6 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
-
 
 class FileStorage:
     """serializes instances to a JSON file & deserializes back to instances"""
@@ -45,6 +42,6 @@ class FileStorage:
             with open(self.__file_path, 'r') as f:
                 jo = json.load(f)
             for key in jo:
-                self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
+                self.__objects[key] = eval(jo[key])["__class__"])(**jo[key])
         except:
             pass
